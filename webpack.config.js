dc8888c2c89b6b6d20c4d02761c3ca33f6c36939
Natalia <nkapelist@gmail.com>
2018-07-20 15:17:01 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+module.exports = {
+	entry: {
+		main: path.resolve(__dirname,'src','index.js')
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname,'build')
+	},
+	watch: true,
+	module: {
+		rules: [
+		{
+			test: /\.css$/,
+			use: [
+				{loader: 'css-loader'}
+			]
+		},
+		{
+			test: /\.scss$/,
+			use: [
+				{loader: 'style-loader'},
+				{loader: 'css-loader'},
+				{loader: 'sass-loader'}
+			]
+		},
+		{
+			test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8000, // Convert images < 8kb to base64 strings
+                            //  name: 'images/[hash]-[name].[ext]'
+                        }
+                    }
+                ]
+        },
+        {
+        	test: /\.(woff|woff2|eot|ttf|otf)$/,
+			loader: "file-loader",  
+        },
+		]
+	},
+	stats: {
+		children: false,
+        chunks: false,
+        colors: true,
+        depth: false,
+        entrypoints: false,
+        errors: true,
+        errorDetails: true,
+        hash: true,
+        modules: false,
+        maxModules: 15,
+        modulesSort: "field",
+        performance: true,
+        timings: true,
+        version: true,
+        warnings: false
+	},	
+	plugins: [
+		new HtmlWebpackPlugin({template:'./assets/index.template.html'}),
+		new CopyWebpackPlugin([
+			{ from: 'assets/img/*.*'},
+			// { from: 'fonts/*.*'}
+		]),
+		new webpack.ProvidePlugin({
+		    $: 'jquery',
+		    jQuery: 'jquery',
+		    'window.jQuery': 'jquery'
+		}),
+		new SWPrecacheWebpackPlugin({
+			cacheId: 'portfolio' + Math.random(),
+			filename: 'service-worker.js',
+			staticFileGlobs: ['build/**.*'],
+			minify: false,
+			stripPrefix: 'build/'
+		})
+	]
+}
